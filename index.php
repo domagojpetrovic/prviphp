d3fa9dcc58501c98c8f8f0b7a3f0236a3a961700
Domagoj Petrovic <domagoj.petrovic1@skole.hr>
2016-03-09 13:36:55 +0100

    Drugi Commit

--- index.php
+++ index.php
@@ -2,11 +2,24 @@
 <html>
 <head>
     <title>Prvi php</title>
+    <meta charset="utf-8"
 </head>
 
 <body>
 <h1>Prvi PHP</h1>
 
+<?php
+    echo('Ovo je prije odlomka<br>');
+    echo('Super, jos cemo dodati<br>');
+
+    echo(date('d.m.Y'));
+    echo('<br>');
+
+    $ocjena = 5;
+    echo('<br>Vaša ocjena je: ' . $ocjena);
+    $nova_var = 166.8;
+    echo('<br>' . $nova_var);
+?>
 <p>
     Ovo je odlomak
 </p>
